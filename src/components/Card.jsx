c2d1dda1c78320a6101ff6508df833a4384d78a8
Martin Castillo <martin.castillo.francino@alumnos.uta.cl>
2025-07-07 08:35:07 -0400

    Subiendo carpeta al repositorio AtlasGym

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+function Card() {
+  return (
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Título carta</h5>
+        <p class="card-text">Texto de ejemplo.</p>
+        <a href="#" class="btn btn-primary">
+          Entrar
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
